227884b576b457041605eaff210396b8124b1136
polinagrushina <143371627+polinagrushina@users.noreply.github.com>
2025-10-16 08:20:23 -0400

    change2

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def get_purchase_code():
 
     if part == "Yes":
         part_type = st.selectbox("Select the type:", ["Non-billable", "Billable", "Contracted"])
-        return {"Non-billable": "60082A", "Billable": "60081", "Contracted": "60082B"}[part_type]
+        return {"Non-billable": "Parts: Non-Billable (Internal) 60082A", "Billable": "Parts: Billable 60081", "Contracted": "Parts: Contracted 60082B"}[part_type]
 
     else:
         vendor_supply = st.radio("Is this for a vendor repair?", ["Yes", "No"])
 
         if vendor_supply == "Yes":
             vendor_type = st.selectbox("Is this billable or non-billable?", ["Billable", "Non-billable"])
-            return {"Billable": "63122", "Non-billable": "63123"}[vendor_type]
+            return {"Billable": "Vendor Repairs: Billable 63122", "Non-billable": "Vendor Repairs: Non-Billable 63123"}[vendor_type]
 
         else:
             biomed_equip = st.radio("Is this for biomed equipment?", ["Yes", "No"])
 
             if biomed_equip == "Yes":
                 biomed_type = st.selectbox("Type of biomed equipment:", ["<$5000", ">$5000", "Furniture<$5000"])
-                return {"<$5000": "65002", ">$5000": "65007", "Furniture<$5000": "65051"}[biomed_type]
+                return {"<$5000": "Equipment <$5000 65002", ">$5000": "Equipment >$5000 65007", "Furniture<$5000": "Equipment: Furniture <$5000 65051"}[biomed_type]
 
             else:
                 misc_equip = st.radio("Is this for certifications, awards, or subscriptions?", ["Yes", "No"])
@@ -29,10 +29,10 @@ def get_purchase_code():
                 if misc_equip == "Yes":
                     misc_type = st.selectbox("Select the type:", ["Certification", "Award", "Membership sub", "Magazine sub"])
                     return {
-                        "Certification": "64039",
-                        "Award": "84000",
-                        "Membership sub": "60561",
-                        "Magazine sub": "60163"
+                        "Certification": "Certification: Tests and Renewals 64039",
+                        "Award": "Sponsored Award 84000",
+                        "Membership sub": "Subscription: Memberships 60561",
+                        "Magazine sub": "Subscription: Magazines 60163"
                     }[misc_type]
 
                 else:
@@ -43,12 +43,12 @@ def get_purchase_code():
                             "Book", "Office staples", "Shipping", "Vehicle maintenance", "Consumables", "Computing"
                         ])
                         return {
-                            "Book": "60161",
-                            "Office staples": "60001",
-                            "Shipping": "60123",
-                            "Vehicle maintenance": "63128",
-                            "Consumables": "63127",
-                            "Computing": "60005"
+                            "Book": "Books 60161",
+                            "Office staples": "Supplies: Office Staples 60001",
+                            "Shipping": "Supplies: Shipping 60123",
+                            "Vehicle maintenance": "Supplies: Vehicle Maintence 63128",
+                            "Consumables": "Supplies: Technician Consumables 63127",
+                            "Computing": "Supplies: Computing 60005"
                         }[gen_type]
                     else:
                         st.error("No matching category found.")
